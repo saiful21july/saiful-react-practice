60085d6f6fd5f996c0f4f25f6c83b40bfaefe76d
saiful <saiful21july@gmail.com>
2021-02-21 14:07:45 +0200

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// class base component
+//import React from 'react'
+// through the props we can handle anything we want for example if we want change the title of a header then retirieve this inside of our props and then we can pass through in our component props as an argument and change the header dynamically.
+// even we can also have default props.
+// even we can put directly title as an argument in our Header component with curly braces and it will give us the same result and at the same time the code will be more robust as well.
+import Header from './components/Header'
 
 function App() {
+  const name = "Saiful"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>this is Saiful</h1>
+      <h2>i am {name}</h2>
+      <Header title = 'Hello Saiful'/>
+      <Header />
     </div>
   );
 }
+
+/*class App extends React.Component {
+  render() {
+    return <h1> hello from a class</h1>
+  }
+}*/
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+
+
+const Header = (props) => {
+    return (
+        <header>
+       <button>Add</button>
+      
+        </header>
+    )
+}
+
+export default Header
+/*const Header = ({title}) => {
+    return (
+        <header>
+          <h1 style={{color:'red'}} >{title}</h1>
+          <button>Add</button>
+        </header>
+    )
+}
+
+export default Header*/
